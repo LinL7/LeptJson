7ede8d03edd641b6708bd53059ece6a9ca9ec230
LinL7 <15823257351@163.com>
2023-01-05 18:26:24 -0800

    null&bool

--- LeptJ/leptjson.h
+++ LeptJ/leptjson.h
@@ -1,7 +1,6 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
-//JSON 中数据类型
 typedef enum {
 LEPT_NULL, 
 LEPT_FALSE,
@@ -12,15 +11,19 @@ LEPT_ARRAY,
 LEPT_OBJECT
 }lept_type;
 
-//JSON中数据结构
+enum {
+    LEPT_PARSE_OK = 0,
+    LEPT_PARSE_EXPECT_VALUE,
+    LEPT_PARSE_INVALID_VALUE,
+    LEPT_PARSE_ROOT_NOT_SINGULAR
+};
+
 typedef struct {
     lept_type type;
 }lept_value;
 
-//JSON解析  
 int lept_parse(lept_value* v, const char* json);
 
-//
 lept_type lept_get_type(const lept_value* v);
 
 #endif 
